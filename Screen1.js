f3930e120e07e9b17152248578d7512bbc00b59d
aliasgharkarani <aliasgharkarani125@hotmail.com>
2020-01-27 07:49:55 +0500

    Invalid app ID

--- Screen1.js
+++ Screen1.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-
+import auth from '@react-native-firebase/auth';
 export default class HomeScreen extends React.Component {
+  async componentDidMount() {
+        const confirmation = await auth().signInWithPhoneNumber('+91 304-205-4458');
+        console.log(confirmation, 'ohh shh');
+  }
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
--- index.js
+++ index.js
@@ -8,13 +8,13 @@ import { name as appName } from './app.json';
 import firebase from '@react-native-firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD0CsC2yspi0TRxCIYxymjL0DW6ypseQWw",
+    piKey: "AIzaSyD0CsC2yspi0TRxCIYxymjL0DW6ypseQWw",
     authDomain: "fir-phoneauth-9e74f.firebaseapp.com",
     databaseURL: "https://fir-phoneauth-9e74f.firebaseio.com",
     projectId: "fir-phoneauth-9e74f",
     storageBucket: "fir-phoneauth-9e74f.appspot.com",
     messagingSenderId: "197117142474",
-    appId: "1:197117142474:web:df029d78cf05c1fac25b33"
+    appId: "1:197117142474:android:a8d5acc4c7ab7e8dc25b33"
 };
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
