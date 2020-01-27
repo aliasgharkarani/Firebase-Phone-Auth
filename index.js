748309bcdc6159442545295c765fd7a3b5084314
aliasgharkarani <aliasgharkarani125@hotmail.com>
2020-01-27 07:53:53 +0500

    Invalid appId

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
     piKey: "AIzaSyD0CsC2yspi0TRxCIYxymjL0DW6ypseQWw",
     authDomain: "fir-phoneauth-9e74f.firebaseapp.com",
     databaseURL: "https://fir-phoneauth-9e74f.firebaseio.com",
-    projectId: "fir-phoneauth-9e74f",
+    projectId: "fir-phoneauth-9e74f", 
     storageBucket: "fir-phoneauth-9e74f.appspot.com",
     messagingSenderId: "197117142474",
     appId: "1:197117142474:android:a8d5acc4c7ab7e8dc25b33"
